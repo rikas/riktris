56b03361e65087c508516cd182d33fa6b0593327
rikas <oterosantos@gmail.com>
2020-11-23 15:53:57 +0000

    Include utils

--- src/main.c
+++ src/main.c
@@ -13,14 +13,6 @@
 #include "gfx.h"
 #include "config.h"
 
-void must_init(bool test, const char *description)
-{
-    if(test) return;
-
-    printf("couldn't initialize %s\n", description);
-    exit(1);
-}
-
 int main()
 {
 	// Seed random
