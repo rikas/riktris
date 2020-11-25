72e8cd347e160b565c9ee6eaed80847d050a3fb8
rikas <oterosantos@gmail.com>
2020-11-25 16:04:48 +0000

    Format code

--- src/gfx.c
+++ src/gfx.c
@@ -10,25 +10,26 @@ void init_gfx()
   gfx->playfield = al_load_bitmap("field.png");
   must_init(gfx->playfield, "gfx playfield.png");
 
-  char* mino_names[TETRIMINO_COUNT] = { "t", "s", "z", "i", "j", "l", "o" };
-  
+  char *mino_names[TETRIMINO_COUNT] = {"t", "s", "z", "i", "j", "l", "o"};
+
   for (int i = 0; i < TETRIMINO_COUNT; i++)
   {
-      char filename[32];
-      
-      sprintf(filename, "mino_%s.png", mino_names[i]);
-      gfx->minos[i] = al_load_bitmap(filename);
-      must_init(gfx->minos[i], filename);
+    char filename[32];
 
-      sprintf(filename, "mino_ghost_%s.png", mino_names[i]);
-      gfx->ghosts[i] = al_load_bitmap(filename);
-      must_init(gfx->ghosts[i], filename);
+    sprintf(filename, "mino_%s.png", mino_names[i]);
+    gfx->minos[i] = al_load_bitmap(filename);
+    must_init(gfx->minos[i], filename);
+
+    sprintf(filename, "mino_ghost_%s.png", mino_names[i]);
+    gfx->ghosts[i] = al_load_bitmap(filename);
+    must_init(gfx->ghosts[i], filename);
   }
 }
 
 void destroy_gfx()
 {
-  if (!gfx) {
+  if (!gfx)
+  {
     return;
   }
 
